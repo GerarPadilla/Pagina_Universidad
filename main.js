2e7decb6cfc26bb43f607784b2bf1b895e98975e
GerardoPadilla <gerardoluispadilla96@gmail.com>
2022-10-09 12:18:51 -0300

    Boton arriba - Falta Responsive

--- main.js
+++ main.js
@@ -60,6 +60,20 @@ function typewriter() {
         setTimeout("typewriter()", iSpeed);
     }
 }
-
-
 typewriter();
+
+/* Boton para ir hacia arriba */
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 100) {
+        document.querySelector('.go-top-container').classList.add('show');
+    } else {
+        document.querySelector('.go-top-container').classList.remove('show');
+    }
+}
+document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
